afe2dc7c12b1f34600691fbead8c10c37079d3f0
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-06 09:34:28 -0500

    add ingredient calculator into cofee machine

--- src/com/ernesto/Main.java
+++ src/com/ernesto/Main.java
@@ -1,8 +1,20 @@
 package com.ernesto;
 
+import java.util.Scanner;
+
 public class Main {
+    final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        final int waterPerCup = 200;  // ml
+        final int milkPerCup = 50;  // ml
+        final int coffeePerCup = 15;  // g
+        System.out.println("Write how many cups of coffee you will need:");
+        int numCups = scanner.nextInt();
+
+        System.out.println("For " + numCups + " cups of coffee you will need: \n" +
+                numCups * waterPerCup + " ml of water\n" +
+                numCups * milkPerCup + " ml of milk\n" +
+                numCups * coffeePerCup + " g of coffee beans");
     }
 }
