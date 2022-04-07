44f5b69d80a195d24ea77f21cd2884a01da6a5df
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-06 23:07:29 -0500

    add sumOfElements()

--- src/WhileAndDoWhileLoops/Exercises.java
+++ src/WhileAndDoWhileLoops/Exercises.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
 //        maxDivisibleByFour();
-        largestOfSequence();
+//        largestOfSequence();
+//        sumOfElements();
+        lengthOfSequence();
+
     }
 
     /*
@@ -56,4 +59,43 @@ public class Exercises {
         System.out.println(max);
     }
 
+    /*
+    The sum of elements
+    Find the sum of all elements of a sequence, ending with the number 0.
+
+    The number 0 itself is not included into the sequence and serves as a sign of cessation.
+     */
+
+    public static void sumOfElements() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int number;
+
+        do {
+            number = scanner.nextInt();
+            sum += number;
+        } while (number != 0);
+
+        System.out.println(sum);
+    }
+
+    /*
+    The length of the sequence
+    For its input, the program gets a sequence of non-negative integers; each integer is written in a separate line.
+    The sequence ends with an integer 0; when the program reads 0, it should end its work and output the length of
+    the sequence (not counting the final 0).
+
+    Don’t read numbers following the number 0.
+     */
+    public static void lengthOfSequence() {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int number;
+
+        while ((number = scanner.nextInt()) != 0) {
+            count++;
+        }
+
+        System.out.println(count);
+    }
 }
