3decda7d27c7284a1313db43a4a95c479bc77570
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-08 15:35:39 -0500

    add busTour()

--- src/BranchingStatements/Exercises.java
+++ src/BranchingStatements/Exercises.java
@@ -7,7 +7,9 @@ public class Exercises {
 
     public static void main(String[] args) {
 //        evenOrOdd();
-        integerBarrier();
+//        integerBarrier();
+//        busTour();
+        theSequence();
     }
 
     /*
@@ -56,7 +58,7 @@ public class Exercises {
         final int barrier = 1_000;
 
         int sum = 0;
-        
+
         while (scanner.hasNextInt()) {
             int number = scanner.nextInt();
             if (number == 0 || sum >= barrier) {
@@ -66,5 +68,59 @@ public class Exercises {
         }
 
         System.out.println(sum >= barrier ? sum - barrier : sum);
+    }
+
+    /*
+    BUS TOUR
+    A bus tour of Europe has been very successful. Due to an increase in the number of people who want to go on a tour,
+    the tour company decided to increase the height of the bus. The new height of the bus is exactly N centimeters.
+
+    But the tour’s route runs under a lot of bridges, and there is a chance that the bus will crash into one of these
+    bridges. Can you find out if this will happen?
+
+    The first line of the input contains the height of the bus and the number of bridges under which the bus passes.
+    The second line contains the heights of these bridges.
+
+    You should output "Will not crash" if everything will be alright; otherwise,
+    output "Will crash on bridge i" (where i is the number of the bridge) into which the bus will crash.
+    If the height of a bridge equals the height of the bus, the bus will crash.
+     */
+
+    public static void busTour() {
+        int busHeight = scanner.nextInt();
+        int bridges = scanner.nextInt();
+        int bridgeHeight;
+        boolean willCrash = false;
+        int i = 1;
+
+        for (; i <= bridges; i++) {
+            bridgeHeight = scanner.nextInt();
+            if (bridgeHeight <= busHeight) {
+                willCrash = true;
+                break;
+            }
+        }
+
+        System.out.println(willCrash ? "Will crash on bridge " + i : "Will not crash");
+    }
+
+    /*
+    THE SEQUENCE
+    Write a program that prints a part of the sequence 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 ...
+    (the number is repeated as many times, to what it equals to).
+    The input to the program is a positive integer n: the number of the elements of the sequence the program should print.
+    Output the sequence of numbers, written in a single line, space-separated.
+
+    For example, if n = 7, then the program should output 1 2 2 3 3 3 4.
+     */
+
+    public static void theSequence() {
+        int number = 5;
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
