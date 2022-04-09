e097e28ac2144f9fd32ad94f40c084e35b8890b9
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-08 23:18:50 -0500

    add stringToNumber()

--- src/SwitchStatement/Exercises.java
+++ src/SwitchStatement/Exercises.java
@@ -1,5 +1,6 @@
 package SwitchStatement;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercises {
@@ -7,7 +8,8 @@ public class Exercises {
 
     public static void main(String[] args) {
 //        direction2();
-        shape();
+//        shape();
+        stringToNumber();
 
     }
 
@@ -87,4 +89,47 @@ public class Exercises {
                 System.out.println("There is no such shape!");
         }
     }
+
+    /*
+    FROM STRING TO NUMBER
+    Write a program that reads a string from the standard input and outputs the number.
+    A number can be from 1 to 9 (inclusive).
+     */
+    public static void stringToNumber() {
+        String input = scanner.nextLine();
+
+        switch (input) {
+            case "one":
+                System.out.println("1");
+                break;
+            case "two":
+                System.out.println("2");
+                break;
+            case "three":
+                System.out.println("3");
+                break;
+            case "four":
+                System.out.println("4");
+                break;
+            case "five":
+                System.out.println("5");
+                break;
+            case "six":
+                System.out.println("6");
+                break;
+            case "seven":
+                System.out.println("7");
+                break;
+            case "eight":
+                System.out.println("8");
+                break;
+            case "nine":
+                System.out.println("9");
+                break;
+            default:
+                System.out.println();
+                break;
+        }
+    }
+
 }
