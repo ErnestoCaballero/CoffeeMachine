5ce593dc2a43bb8e81584d4e9d75f7b4024e46e9
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-12 16:16:52 -0500

    add MultipleFunction class

---
+++ src/FunctionalDecomposition/MultipleFunction.java
@@ -0,0 +1,35 @@
+package FunctionalDecomposition;
+
+import java.util.Scanner;
+
+public class MultipleFunction {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        System.out.println(f(x));
+    }
+
+    public static double f(double x) {
+        if (x <= 0) {
+            return f1(x);
+        } else if (x >= 1) {
+            return f3(x);
+        } else {
+            return f2(x);
+        }
+
+    }
+
+    public static double f1(double x) {
+        return Math.pow(x, 2) + 1;
+    }
+
+    public static double f2(double x) {
+        return 1 / (Math.pow(x, 2));
+    }
+
+    public static double f3(double x) {
+        return Math.pow(x, 2) - 1;
+    }
+}
