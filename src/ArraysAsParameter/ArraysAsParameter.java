807cf0ff55202c97c699761090bc989b2873823a
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-13 13:10:07 -0500

    add inverseFlags()

--- src/ArraysAsParameter/ArraysAsParameter.java
+++ src/ArraysAsParameter/ArraysAsParameter.java
@@ -2,6 +2,18 @@ package ArraysAsParameter;
 
 public class ArraysAsParameter {
     public static void main(String[] args) {
-        
+        inverseFlags(new boolean[] {true, false});
+    }
+
+    /*
+    INVERSE BOOLEAN FLAGS
+    Write a body and a parameter of the method inverseFlags. The method must take an array of booleans
+    and negate each of them. Do not make a copy of the parameter, change elements of a passed array.
+     */
+
+    public static void inverseFlags(boolean[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = !arr[i];
+        }
     }
 }
