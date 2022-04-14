02500d7c7bffff1805b7ee07747dae702bdf2f89
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-14 13:36:16 -0500

    add triples()

--- src/ArraysAsParameter/ArraysAsParameter.java
+++ src/ArraysAsParameter/ArraysAsParameter.java
@@ -9,7 +9,8 @@ public class ArraysAsParameter {
 //        rotateArray();
 //        isContain();
 //        indexOfMax();
-        indexOfMax2();
+//        indexOfMax2();
+        triples();
 
 
     }
@@ -350,5 +351,42 @@ public class ArraysAsParameter {
         }
 
         System.out.println(maxIndex);
+    }
+
+    /*
+    TRIPLES
+
+    Write a program that reads an array of ints and outputs the number of "triples" in the array.
+
+    A "triple" is three consecutive ints in increasing order differing by 1 (i.e. 3,4,5 is a triple, but 5,4,3 and 2,4,6 are not). Triples may overlap. For example, the sequence 1, 2, 3, 4, 5 contains three triples: 1, 2, 3; 2, 3, 4; and 3, 4, 5.
+
+    Input data format
+
+    The first line contains the size of an array.
+    The second line contains elements of the array separated by spaces.
+
+    Output data format
+
+    Only a single integer value: the number of "triples".
+     */
+
+    public static void triples() {
+        Scanner scanner = new Scanner(System.in);
+        int[] input = new int[scanner.nextInt()];
+        populateArray(input, scanner);
+        int triples = 0;
+
+        for (int i = 0; i < input.length - 2; i++) {
+            if (input[i + 1] - input[i] == 1 && input[i + 2] - input[i + 1] == 1) {
+                triples++;
+            }
+        }
+        System.out.println(triples);
+    }
+
+    public static void populateArray(int[] arr, Scanner scanner) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
     }
 }
