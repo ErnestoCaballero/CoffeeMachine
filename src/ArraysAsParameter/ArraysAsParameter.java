a69e0dcd08c028486915d5ad9642095a55cd8c95
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-15 18:43:02 -0500

    test1

--- src/ArraysAsParameter/ArraysAsParameter.java
+++ src/ArraysAsParameter/ArraysAsParameter.java
@@ -14,7 +14,8 @@ public class ArraysAsParameter {
 //        containTwoNumbers();
 //        isAscending();
 //        isAscending2();
-        maxAdjacentProduct();
+//        maxAdjacentProduct();
+        individualTaxes();
 
 
     }
@@ -508,5 +509,42 @@ public class ArraysAsParameter {
         }
 
         System.out.println(maxProd);
+    }
+
+    /*
+    INDIVIDUAL TAXES
+    In some country, there are N companies, and the law of that country says that the taxes of each company are individual
+    and appointed by the president. The president wants to know which company pays the most taxes.
+    But sadly, none of the president's proxies know math very well, so this work is transferred to you.
+    Can you solve this problem?
+
+    The first line of the input contains N which is the number of companies in the country.
+    The second line contains the yearly incomes of each company. All numbers are non-negative integers.
+    The third line contains individual taxes for each company in percent of the company's income.
+    All numbers are integers from 0 to 100 inclusive.
+
+    You should output the number of the company that pays the most taxes.
+    Keep in mind that the enumeration of the companies starts with number 1.
+    If there are several companies with the same payment sizes, output the number of the company with the lowest number.
+
+    Note: be careful not to lose fractional parts while performing your calculations.
+    All input and output numbers are integers, but you may have to use a different type for storing intermediate values.
+     */
+    public static void individualTaxes() {
+        Scanner scanner = new Scanner(System.in);
+        final double percentage = 100;
+        int len = scanner.nextInt();
+        int[] companies = new int[len];
+        int[] taxRate = new int[len];
+        populateArray(companies);
+        populateArray(taxRate);
+
+        double[] totalTaxes = new double[len];
+        for (int i = 0; i < len; i++) {
+            totalTaxes[i] = companies[i] * taxRate[i] / percentage;
+        }
+
+
+
     }
 }
