fffb74d52291d1ba92dc0d413dca89cd40bf8dfa
Ernesto Caballero Delgado <ernestca@amdocs.com>
2022-04-17 21:09:55 -0500

    finish Clock class

--- src/InstanceMethods/Clock.java
+++ src/InstanceMethods/Clock.java
@@ -1,4 +1,14 @@
 package InstanceMethods;
 
-public class Clock {
+class Clock {
+    int hours = 12;
+    int minutes = 58;
+
+    void next() {
+       if (++this.minutes > 59) {
+            this.hours = this.hours == 12 ? 1 : this.hours + 1;
+            this.minutes = 0;
+        }
+    }
+
 }
--- src/InstanceMethods/Main.java
+++ src/InstanceMethods/Main.java
@@ -2,6 +2,15 @@ package InstanceMethods;
 
 public class Main {
     public static void main(String[] args) {
+        Clock clock = new Clock();
+        printStatus(clock);
+        clock.next();
+        printStatus(clock);
+    }
 
+    static void printStatus(Clock clock) {
+        System.out.println();
+        System.out.println(clock.hours);
+        System.out.println(clock.minutes);
     }
 }
